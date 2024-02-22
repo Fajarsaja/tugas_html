335e272dc05c5b77279a25645aea06a34c738c73
fajar <ikhromfajar2@gmail.com>
2024-02-22 19:05:37 +0700

    add tugas

---
+++ page.js
@@ -0,0 +1,10 @@
+let btn = document.getElementById("btn")
+
+
+function move1(){
+    window.location.href = "informasi_siswa.html"
+}
+function move2(){
+    window.location.href = "index.html"
+
+}
